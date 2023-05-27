03116d0a0b26d61c928bb9b15e058c1dd44ee20f
edwinolivares24@gmail.com <edwinolivares24@gmail.com>
2023-05-26 22:03:18 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(names) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(names) {
+ cats.pop();   
+}
+function destructivelyRemoveFirstCat(names) {
+    cats.shift();
+}
+function appendCat(names) {
+    return [...cats, "Broom"];
+}
+function prependCat(name) {
+    return [ "Arnold", ...cats];
+}
+function removeLastCat(name) {
+    return cats.slice(0, -1);
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
